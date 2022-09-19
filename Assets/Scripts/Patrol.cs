09de1ac4515fff087f718bc1bae390acb6f3b450
AD\u1330244 <debdipbanerjee7@gmail.com>
2022-09-19 13:50:53 -0600

    Enemy patrol added

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float speed;
+    int currentPointIndex;
+
+    float waitTime;
+    public float startWaitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = patrolPoints[0].position;
+        waitTime = startWaitTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, patrolPoints[currentPointIndex].position, speed * Time.deltaTime);
+
+        if(transform.position == patrolPoints[currentPointIndex].position)
+        {
+            if (waitTime <= 0)
+            {
+                if (currentPointIndex + 1 < patrolPoints.Length)
+                {
+                    currentPointIndex++;
+                }
+                else
+                {
+                    currentPointIndex = 0;
+                }
+
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+
+           
+
+        }
+    }
+}
